4398d43e451a0a0bde0c1ca55f3ac5452c523276
toppo0323 <toppo9118@gmail.com>
2021-09-04 19:23:53 +0000

    add recipe controller

---
+++ app/controllers/public/recipes_controller.rb
@@ -0,0 +1,55 @@
+class Public::RecipesController < ApplicationController
+  
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.customer_id = current_customer.id
+    @recipe.save
+    redirect_to recipes_path
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    if @recipe.customer == current_customer
+        render "edit"
+    else
+        redirect_to recipes_path
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.customer_id = current_customer.id
+     if @recipe.update(recipe_params)
+       flash[:succes]= "You have created book successfully."
+       redirect_to recipe_path(@recipe.id)
+     else
+       flash[:notice]= 'errors prohibited this obj from being saved:'
+       render "edit"
+     end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name,:image,:ingredient,:process)
+  end
+  
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '14f0408c8cbbdd1986c2711a3efb719fee381c084368aa608369667a751c0c28094994c710677f8881abfc1d7fabf43b212ba75975e51d9d9a761f02aaf35cc2'
