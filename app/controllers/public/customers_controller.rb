252d665e4c64af3e6bd19003e16558679810cebd
toppo0323 <toppo9118@gmail.com>
2021-09-04 13:14:47 +0000

    add devise and customer

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,25 @@
+class Public::CustomersController < ApplicationController
+
+  before_action :authenticate_customer!, except: [:contact]
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
+  end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:name,:email)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
 
 
+
+
+ devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'homes#top'
+
+
+scope module: :public do
+    root 'homes#top'
+
+    resources :customers,only:[:show,:edit,:update]
+
+
+
+end
+
 
 end
