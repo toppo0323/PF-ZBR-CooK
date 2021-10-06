3ac5a12a5e325ccdbd4cbec4229e7c6bf596be48
toppo0323 <toppo9118@gmail.com>
2021-10-06 12:30:24 +0000

    comment

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,7 @@
 class Public::SearchesController < ApplicationController
 before_action :authenticate_customer!, except: [:search]
 
+  # keywordにサーチしたいものを代入、検索結果はモデルを参照
   def search
     if params["genre"]
       @recipes = Genre.find(params["genre"]).recipes.all
